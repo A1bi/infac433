b73a77078ac26e908e32f4100ba56ee01d7bd09c
Albrecht Oster <albrecht@oster.online>
2023-09-04 17:11:33 +0200

    Clean up GPIO before exiting

--- infac433.c
+++ infac433.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <lgpio.h>
 #include <unistd.h>
 
@@ -5,19 +6,40 @@
 
 #define PIN_IN 24
 
+uint8_t aborted = 0;
+
+static void exit_handler(int signum) {
+  aborted = 1;
+}
+
+static void setup_handlers() {
+  struct sigaction sa = {
+    .sa_handler = exit_handler,
+  };
+
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
+}
+
 int main(int argc, char *argv[]) {
-  uint8_t h = lgGpiochipOpen(0);
+  setup_handlers();
+
+  uint8_t gpio_handle = lgGpiochipOpen(0);
   uint8_t edge = 0;
   uint8_t val = 0;
 
-  lgGpioClaimInput(h, LG_SET_PULL_DOWN, PIN_IN);
+  lgGpioClaimInput(gpio_handle, LG_SET_PULL_DOWN, PIN_IN);
 
-  while (1) {
-    val = lgGpioRead(h, PIN_IN);
+  while (!aborted) {
+    val = lgGpioRead(gpio_handle, PIN_IN);
     if (val != edge) {
       edge = val;
       infac_decoder_process_pulse(val);
     }
     usleep(100);
   }
+
+  lgGpioFree(gpio_handle, PIN_IN);
+
+  return 0;
 }
